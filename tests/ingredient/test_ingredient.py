36df2a71c386540c43abbe941035c06d86080251
Helton-Pereira <helton.bfsm@gmail.com>
2023-05-16 14:54:37 -0300

    Req 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ...
+    presunto = Ingredient("presunto")
+
+    assert presunto.name == "presunto"
+
+    assert presunto.__hash__() == hash("presunto")
+
+    assert presunto.__repr__() == "Ingredient('presunto')"
+
+    assert presunto.__eq__("salmão") is False
+
+    assert presunto == Ingredient("presunto")
+
+    ham_restrictions = {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+
+    assert presunto.restrictions == ham_restrictions
